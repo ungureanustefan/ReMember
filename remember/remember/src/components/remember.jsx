7f255c791901c8b87e0badd529b843233c226668
Stefan Ungureanu <ungureanustefan680@gmail.com>
2020-08-28 15:28:56 +0100

    Multiple changes/ Initial app created/ React intro

---
+++ remember/remember/src/components/remember.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Html extends Component {
+  state = {};
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+export default Html;
---
+++ remember/remember/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Remember from "./components/remember";
+
+ReactDOM.render(<Remember />, document.getElementById("root"));
