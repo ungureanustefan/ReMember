a3ef534dc4cb2a0c7a9b12fc26bfd756b56ef8c1
Stefan <ungureanustefan680@gmail.com>
2021-02-03 13:22:28 +0000

    Updates

---
+++ precache-manifest.e9fcce50d1ff971d04a45879f4a24a6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c952fec2f21e97c754bad2e008b53d3",
+    "url": "/ReMember/index.html"
+  },
+  {
+    "revision": "7c5b10abca38d22c118b",
+    "url": "/ReMember/static/css/main.8722db2f.chunk.css"
+  },
+  {
+    "revision": "4c4164629ae42036d7a2",
+    "url": "/ReMember/static/js/2.10860725.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReMember/static/js/2.10860725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5b10abca38d22c118b",
+    "url": "/ReMember/static/js/main.be3e4bbd.chunk.js"
+  },
+  {
+    "revision": "bb7b2fb3dbd5d417bac2",
+    "url": "/ReMember/static/js/runtime-main.adba0c85.js"
+  }
+]);
